6ddb3fd9e6d55321658a38b2df28528a40b02a84
Sebastian Osiński <seb.osinski@gmail.com>
2017-01-22 21:07:27 +0100

    Initial Commit

---
+++ SubtitlesDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubtitlesDownloader
+//
+//  Created by Sebastian Osiński on 22/01/2017.
+//  Copyright © 2017 Sebastian Osinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
