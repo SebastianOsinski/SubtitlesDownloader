9a02278dcd532c10a91d6bb6d06d44b24eca0a23
Sebastian Osiński <seb.osinski@gmail.com>
2017-01-23 22:19:21 +0100

    Add FileCellProtocol

---
+++ SubtitlesDownloader/Presentation/FileCellProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  FileCellProtocol.swift
+//  SubtitlesDownloader
+//
+//  Created by Sebastian Osiński on 23/01/2017.
+//  Copyright © 2017 Sebastian Osiński. All rights reserved.
+//
+
+import Foundation
+
+protocol FileCell {
+
+    func displayName(_ name: String)
+}
