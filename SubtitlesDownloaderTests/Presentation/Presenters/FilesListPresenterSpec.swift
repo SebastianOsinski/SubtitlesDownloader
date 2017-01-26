f170c5bc768ec39e961dcb2ae93efd62f4ca6095
Sebastian Osiński <seb.osinski@gmail.com>
2017-01-26 20:14:41 +0100

    Add more tests for FilesListPresenter

--- SubtitlesDownloaderTests/Presentation/Presenters/FilesListPresenterSpec.swift
+++ SubtitlesDownloaderTests/Presentation/Presenters/FilesListPresenterSpec.swift
@@ -17,18 +17,56 @@ class FilesListPresenterSpec: QuickSpec {
 
         var sut: FilesListPresenter!
         var useCaseFactory: MockUseCaseFactory!
+        var view: MockFilesListView!
 
         beforeEach {
             useCaseFactory = MockUseCaseFactory(fileGateway: DummyFileGateway())
             sut = FilesListPresenter(path: "", useCaseFactory: useCaseFactory)
+            view = MockFilesListView()
+            sut.view = view
         }
 
         describe("FilesListPresenter") {
+
+            it("has no data in the beginning") {
+                expect(sut.numberOfFiles).to(equal(0))
+            }
+
             it("calls use case when view is ready") {
                 sut.viewReady()
                 expect(useCaseFactory.useCase.executeCalled).to(beTrue())
             }
+
+            it("has same amount of data as passed") {
+                useCaseFactory.data = [
+                    File(name: "A", path: "A", type: .regular),
+                    File(name: "B", path: "B", type: .regular)
+                ]
+
+                sut.viewReady()
+
+                expect(sut.numberOfFiles).to(equal(useCaseFactory.data.count))
+            }
+
+            it("calls refresh on view when view is ready") {
+                sut.viewReady()
+                expect(view.refreshCalled).to(beTrue())
+            }
         }
+    }
+}
+
+private class MockFilesListView: FilesListViewProtocol {
+
+    private(set) var refreshCalled = false
+    private(set) var shownDirectoryPath: String?
+
+    func refresh() {
+        refreshCalled = true
+    }
+
+    func showDirectory(atPath path: String) {
+        shownDirectoryPath = path
     }
 }
 
