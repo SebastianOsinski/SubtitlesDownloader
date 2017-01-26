de1716cdb0ccd994861914201be4c60b7dfb62ea
Sebastian Osiński <seb.osinski@gmail.com>
2017-01-26 19:58:03 +0100

    Add FilesListPresenterSpec

---
+++ SubtitlesDownloaderTests/Presentation/Presenters/FilesListPresenterSpec.swift
@@ -0,0 +1,92 @@
+//
+//  FilesListPresenterSpec.swift
+//  SubtitlesDownloader
+//
+//  Created by Sebastian Osiński on 26/01/2017.
+//  Copyright © 2017 Sebastian Osiński. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import SubtitlesDownloader
+
+class FilesListPresenterSpec: QuickSpec {
+
+    override func spec() {
+
+        var sut: FilesListPresenter!
+        var useCaseFactory: MockUseCaseFactory!
+
+        beforeEach {
+            useCaseFactory = MockUseCaseFactory(fileGateway: DummyFileGateway())
+            sut = FilesListPresenter(path: "", useCaseFactory: useCaseFactory)
+        }
+
+        describe("FilesListPresenter") {
+            it("calls use case when view is ready") {
+                sut.viewReady()
+                expect(useCaseFactory.useCase.executeCalled).to(beTrue())
+            }
+        }
+    }
+}
+
+private class MockUseCaseFactory: UseCaseFactory {
+
+    var data = [File]()
+    private(set) var useCase: MockShowFilesListUseCase!
+
+    override func createUseCase(for use: Use) -> UseCase {
+        switch use {
+        case .showFiles(let path, let completion):
+            useCase = MockShowFilesListUseCase(
+                directoryPath: path,
+                gateway: DummyFileGateway(),
+                completion: completion
+            )
+
+            useCase.data = data
+
+            return useCase
+        default:
+            fatalError()
+        }
+    }
+
+}
+
+private class MockShowFilesListUseCase: UseCase {
+
+    private(set) var executeCalled = false
+
+    private let directoryPath: String
+    private let gateway: FileGateway
+    private let completion: ShowFilesListCompletion
+
+    var data = [File]()
+
+    init(directoryPath: String, gateway: FileGateway, completion: @escaping ShowFilesListCompletion) {
+        self.directoryPath = directoryPath
+        self.gateway = gateway
+        self.completion = completion
+    }
+
+    func execute() -> UseCaseHandle? {
+        executeCalled = true
+        completion(.success(data))
+        return nil
+    }
+
+}
+
+private class DummyFileGateway: FileGateway {
+
+    func contentsOfDirectory(path: String, completion: @escaping (Result<[File]>) -> Void) -> OperationHandle? {
+        return nil
+    }
+
+    func contents(path: String, offset: Int64, length: Int, completion: @escaping (Result<Data>) -> Void) -> OperationHandle? {
+        return nil
+    }
+}
